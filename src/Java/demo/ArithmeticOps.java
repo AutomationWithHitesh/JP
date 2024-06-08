1a794c330f62b6e4a10de288122ec704d651608d
AutomationWithHitesh <hiteshdkhatate@gmail.com>
2024-06-08 22:51:31 +0530

    arithmetic ops

---
+++ src/Java/demo/ArithmeticOps.java
@@ -0,0 +1,40 @@
+package Java.demo;
+
+public class ArithmeticOps {
+	public static void main(String[] args) {
+
+
+		// TODO Auto-generated method stub
+		System.out.println("::basic calculator::");
+		int num1=30;
+		int num2=5;
+		System.out.println("Addition:");
+		System.out.println("number 1:"+num1);
+		System.out.println("number 2:"+num2);
+		int sum=num1+num2;
+		System.out.println("sum:"+sum);
+		int sub1=30;
+		int sub2=5;
+		System.out.println("Subtraction:");
+		System.out.println("number 1:"+sub1);
+		System.out.println("number 2:"+sub2);
+		int sub=sub1-sub2;
+		System.out.println("subtraction:"+sub);
+
+		int mul1=30;
+		int mul12=5;
+		System.out.println("multiplication:");
+		System.out.println("number 1:"+mul1);
+		System.out.println("number 2:"+mul12);
+		int mul=mul1*mul12;
+		System.out.println("multiplication:"+mul);
+		double div1=30;
+		double div2=5;
+		System.out.println("Division:");
+		System.out.println("number 1:"+div1);
+		System.out.println("number 2:"+div2);
+		double div=div1/div2;
+		System.out.println("Division:"+div);
+
+	}
+}

3db28c7a9a33c7366702d88ed211befc919099ad
AutomationWithHitesh <hiteshdkhatate@gmail.com>
2024-06-09 12:51:51 +0530

    success

---
+++ src/Java/demo/SwitchOps.java
@@ -0,0 +1,28 @@
+/*
+ * whenever you have 'multiple condition checking' you can use switch case statement 
+ * in place of if else
+ * 
+ */
+
+package Java.demo;
+
+public class SwitchOps {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int choice=1;
+	switch(choice)
+	{
+	case 1:
+		System.out.println("selected case is 1");
+		break;
+	case 2:
+		System.out.println("selected case is 2");
+		break;
+	default:
+		System.out.println("no match found");
+		
+	}
+
+}
+}
